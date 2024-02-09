bda1a205c91017de9a819c8c9dd37f3d9a220976
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-02-09 16:34:30 +0000

    hero image added

--- src/app/pages/hero/page.js
+++ src/app/pages/hero/page.js
@@ -1,5 +1,10 @@
+import navImg from "/public/hero-image.webp";
+import Image from "next/image";
+
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center justify-start h-screen mt-10"></div>
+    <div className="flex flex-col items-center justify-start h-screen mt-10">
+      <Image src={navImg} alt="nav" className="w-full md:w-3/4 lg:w-1/2" />
+    </div>
   );
 }
